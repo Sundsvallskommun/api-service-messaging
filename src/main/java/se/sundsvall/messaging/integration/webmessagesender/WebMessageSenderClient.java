55b954682c30470474a4a11f012ad9076a566c7b
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2022-08-22 10:39:36 +0200

    UF-XYZ - Use Feign client
    Changes the WebMessageSender client to use Feign instead of RestTemplate

---
+++ src/main/java/se/sundsvall/messaging/integration/webmessagesender/WebMessageSenderClient.java
@@ -0,0 +1,18 @@
+package se.sundsvall.messaging.integration.webmessagesender;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import generated.se.sundsvall.webmessagesender.CreateWebMessageRequest;
+
+@FeignClient(
+    name = WebMessageSenderIntegration.INTEGRATION_NAME,
+    url = "${integration.web-message-sender.base-url}",
+    configuration = WebMessageSenderIntegrationConfiguration.class
+)
+public interface WebMessageSenderClient {
+
+    @PostMapping("/webmessages")
+    ResponseEntity<Void> sendWebMessage(CreateWebMessageRequest request);
+}
