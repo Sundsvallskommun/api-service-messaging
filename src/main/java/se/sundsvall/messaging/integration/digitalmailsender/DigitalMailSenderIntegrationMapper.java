c5a8df925a646f66180622d8f6698005ae1cb7aa
lsjolinder <linus.sjolinder@b3.se>
2022-10-26 13:03:42 +0200

    null values should not generate empty lists

--- src/main/java/se/sundsvall/messaging/integration/digitalmailsender/DigitalMailSenderIntegrationMapper.java
+++ src/main/java/se/sundsvall/messaging/integration/digitalmailsender/DigitalMailSenderIntegrationMapper.java
@@ -1,9 +1,11 @@
 package se.sundsvall.messaging.integration.digitalmailsender;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import se.sundsvall.messaging.dto.DigitalMailDto;
 
@@ -26,23 +28,29 @@ class DigitalMailSenderIntegrationMapper {
             return null;
         }
 
+        List<Attachment> attachments = null;
+        if (!CollectionUtils.isEmpty(dto.getAttachments())) {
+            attachments = Optional.ofNullable(dto.getAttachments()).stream()
+                    .flatMap(Collection::stream)
+                    .map(attachment -> new Attachment()
+                            .contentType(attachment.getContentType().getValue())
+                            .body(attachment.getContent())
+                            .filename(attachment.getFilename()))
+                    .toList();
+        }
+
         return new DigitalMailRequest()
-            .partyId(dto.getPartyId())
-            .municipalityId(properties.getDefaults().getMunicipalityId())
-            .headerSubject(Optional.ofNullable(dto.getSubject()).orElse(properties.getDefaults().getSubject()))
-            .supportInfo(new SupportInfo()
-                .supportText(properties.getDefaults().getSupportInfo().getText())
-                .contactInformationEmail(properties.getDefaults().getSupportInfo().getEmailAddress())
-                .contactInformationPhoneNumber(properties.getDefaults().getSupportInfo().getPhoneNumber())
-                .contactInformationUrl(properties.getDefaults().getSupportInfo().getUrl()))
-            .bodyInformation(new BodyInformation()
-                .contentType(dto.getContentType().getValue())
-                .body(dto.getBody()))
-            .attachments(Optional.ofNullable(dto.getAttachments()).orElse(List.of()).stream()
-                .map(attachment -> new Attachment()
-                    .contentType(attachment.getContentType().getValue())
-                    .body(attachment.getContent())
-                    .filename(attachment.getFilename()))
-                .toList());
+                .partyId(dto.getPartyId())
+                .municipalityId(properties.getDefaults().getMunicipalityId())
+                .headerSubject(Optional.ofNullable(dto.getSubject()).orElse(properties.getDefaults().getSubject()))
+                .supportInfo(new SupportInfo()
+                        .supportText(properties.getDefaults().getSupportInfo().getText())
+                        .contactInformationEmail(properties.getDefaults().getSupportInfo().getEmailAddress())
+                        .contactInformationPhoneNumber(properties.getDefaults().getSupportInfo().getPhoneNumber())
+                        .contactInformationUrl(properties.getDefaults().getSupportInfo().getUrl()))
+                .bodyInformation(new BodyInformation()
+                        .contentType(dto.getContentType().getValue())
+                        .body(dto.getBody()))
+                .attachments(attachments);
     }
 }
