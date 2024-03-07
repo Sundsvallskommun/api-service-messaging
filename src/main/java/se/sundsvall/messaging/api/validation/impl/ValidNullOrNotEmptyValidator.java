b8d2e2b1c6acb8b94839917287951736a77494ea
Michael Wallner <Michael.Wallner@Sundsvall.se>
2024-03-07 16:23:46 +0100

    UF-7380: Changes after review

--- src/main/java/se/sundsvall/messaging/api/validation/impl/ValidNullOrNotEmptyValidator.java
+++ src/main/java/se/sundsvall/messaging/api/validation/impl/ValidNullOrNotEmptyValidator.java
@@ -4,8 +4,6 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import se.sundsvall.messaging.api.validation.ValidNullOrNotEmpty;
 
-import static java.util.Objects.isNull;
-
 /**
  * Validator for {@link ValidNullOrNotEmpty} annotation.
  * Checks if a string is null or not empty and has no starting or trailing spaces.
@@ -15,13 +13,6 @@ public class ValidNullOrNotEmptyValidator implements ConstraintValidator<ValidNu
 
 	@Override
 	public boolean isValid(final String value, final ConstraintValidatorContext context) {
-		if (isNull(value)) {
-			return true;
-		}
-		return isValidFormat(value);
-	}
-
-	public boolean isValidFormat(final String value) {
-		return !value.isEmpty() && !value.startsWith(" ") && !value.endsWith(" ");
+		return value == null || (!value.isEmpty() && !value.startsWith(" ") && !value.endsWith(" "));
 	}
 }
