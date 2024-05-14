05274a02018d08317909b672e0f2591b6e7146f2
Martin Hansson <martinhansson80@gmail.com>
2024-05-14 12:34:02 +0200

    uf-8309: Made event handler asynchronous

--- src/main/java/se/sundsvall/messaging/Application.java
+++ src/main/java/se/sundsvall/messaging/Application.java
@@ -2,9 +2,12 @@ package se.sundsvall.messaging;
 
 import static org.springframework.boot.SpringApplication.run;
 
+import org.springframework.scheduling.annotation.EnableAsync;
+
 import se.sundsvall.dept44.ServiceApplication;
 
 @ServiceApplication
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
