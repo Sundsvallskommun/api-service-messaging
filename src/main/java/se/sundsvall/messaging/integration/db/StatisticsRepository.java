49d0ff6343e554b16b72f33e40eee8ec9158f167
cheezi0747 <linus.sjolinder@b3.se>
2024-10-31 07:57:08 +0100

    UF-10709: Fixes sonar issues

--- src/main/java/se/sundsvall/messaging/integration/db/StatisticsRepository.java
+++ src/main/java/se/sundsvall/messaging/integration/db/StatisticsRepository.java
@@ -1,19 +1,17 @@
 package se.sundsvall.messaging.integration.db;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import se.sundsvall.messaging.integration.db.entity.HistoryEntity;
 import se.sundsvall.messaging.integration.db.projection.StatsEntry;
 import se.sundsvall.messaging.model.MessageType;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 @CircuitBreaker(name = "statisticsRepository")
@@ -28,11 +26,11 @@ public interface StatisticsRepository extends JpaRepository<HistoryEntity, Long>
 	}
 
 	@Query("""
-			SELECT NEW StatsEntry(h.messageType, h.originalMessageType, h.status, h.municipalityId) FROM HistoryEntity h WHERE
-			(:message_type IS NULL OR h.originalMessageType = :message_type) AND
-			(:from_date IS NULL OR h.createdAt >= :from_date) AND
-			(:to_date IS NULL OR h.createdAt <= :to_date) AND
-			(:municipality_id IS NULL OR h.municipalityId = :municipality_id)
+		SELECT NEW StatsEntry(h.messageType, h.originalMessageType, h.status, h.municipalityId) FROM HistoryEntity h WHERE
+		(:message_type IS NULL OR h.originalMessageType = :message_type) AND
+		(:from_date IS NULL OR h.createdAt >= :from_date) AND
+		(:to_date IS NULL OR h.createdAt <= :to_date) AND
+		(:municipality_id IS NULL OR h.municipalityId = :municipality_id)
 		""")
 	List<StatsEntry> getStatsQuery(
 		@Param("message_type") final MessageType messageType,
@@ -51,13 +49,13 @@ public interface StatisticsRepository extends JpaRepository<HistoryEntity, Long>
 	}
 
 	@Query(value = """
-			SELECT NEW StatsEntry(h.messageType, h.originalMessageType, h.status, h.origin, h.department, h.municipalityId) FROM HistoryEntity h WHERE
-			(:message_type IS NULL OR h.originalMessageType = :message_type) AND
-			(:from_date IS NULL OR h.createdAt >= :from_date) AND
-			(:to_date IS NULL OR h.createdAt <= :to_date) AND
-			(:department IS NULL OR h.department = :department) AND
-			(:origin IS NULL OR h.origin = :origin) AND
-			(:municipality_id IS NULL OR h.municipalityId = :municipality_id)
+		SELECT NEW StatsEntry(h.messageType, h.originalMessageType, h.status, h.origin, h.department, h.municipalityId) FROM HistoryEntity h WHERE
+		(:message_type IS NULL OR h.originalMessageType = :message_type) AND
+		(:from_date IS NULL OR h.createdAt >= :from_date) AND
+		(:to_date IS NULL OR h.createdAt <= :to_date) AND
+		(:department IS NULL OR h.department = :department) AND
+		(:origin IS NULL OR h.origin = :origin) AND
+		(:municipality_id IS NULL OR h.municipalityId = :municipality_id)
 		""")
 	List<StatsEntry> getStatsByOriginAndDepartmentQuery(
 		@Param("origin") final String origin,
