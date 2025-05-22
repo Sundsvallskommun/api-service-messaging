46f75d4189102c81c1fb0a848d2173157533de81
Martin Hansson <martinhansson80@gmail.com>
2025-05-22 15:14:59 +0200

    uf-16205: New header X-Sent-By, deprecate x-issuer.

--- src/test/java/se/sundsvall/messaging/api/MessageResourceSlackTest.java
+++ src/test/java/se/sundsvall/messaging/api/MessageResourceSlackTest.java
@@ -14,6 +14,7 @@ import static se.sundsvall.messaging.model.MessageStatus.SENT;
 import static se.sundsvall.messaging.model.MessageType.SLACK;
 
 import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,9 @@ class MessageResourceSlackTest {
 	private static final String ORIGIN = "origin";
 	private static final String ISSUER_HEADER = "x-issuer";
 	private static final String ISSUER = "issuer";
+	private static final String X_SENT_BY_HEADER = "X-Sent-By";
+	private static final String X_SENT_BY = "type=adAccount; joe01doe";
+	private static final String X_SENT_BY_VALUE = "joe01doe";
 
 	private static final InternalDeliveryResult DELIVERY_RESULT = InternalDeliveryResult.builder()
 		.withMessageId("someMessageId")
@@ -129,17 +133,58 @@ class MessageResourceSlackTest {
 		verifyNoInteractions(mockMessageService);
 	}
 
+	@Test
+	void testOldHeadersShouldBePreserved() {
+		// Arrange
+		final var request = createValidSlackRequest();
+		when(mockMessageService.sendToSlack(any())).thenReturn(DELIVERY_RESULT);
+
+		// Act
+		final var response = webTestClient.post()
+			.uri(URL)
+			.headers(oldHeaders())
+			.contentType(APPLICATION_JSON)
+			.bodyValue(request)
+			.exchange()
+			.expectHeader().exists(LOCATION)
+			.expectHeader().valuesMatch(LOCATION, "^/" + MUNICIPALITY_ID + "/status/messages/(.*)$")
+			.expectStatus().isCreated()
+			.expectBody(MessageResult.class)
+			.returnResult()
+			.getResponseBody();
+
+		// Assert & verify
+		assertThat(response).isNotNull();
+		assertThat(response.messageId()).isEqualTo("someMessageId");
+		assertThat(response.deliveries()).isNotNull().hasSize(1);
+		assertThat(response.deliveries().getFirst().messageType()).isEqualTo(SLACK);
+		assertThat(response.deliveries().getFirst().deliveryId()).isEqualTo("someDeliveryId");
+		assertThat(response.deliveries().getFirst().status()).isEqualTo(SENT);
+
+		verify(mockMessageService).sendToSlack(request.withOrigin(ORIGIN).withIssuer(ISSUER));
+		verifyNoMoreInteractions(mockEventDispatcher);
+		verifyNoInteractions(mockEventDispatcher);
+	}
+
 	private static Consumer<HttpHeaders> handleHeaders(boolean includeOptionalHeaders) {
 		return httpHeaders -> {
 			if (includeOptionalHeaders) {
 				httpHeaders.add(ORIGIN_HEADER, ORIGIN);
 				httpHeaders.add(ISSUER_HEADER, ISSUER);
+				httpHeaders.add(X_SENT_BY_HEADER, X_SENT_BY);
 			}
+		};
+	}
+
+	private static Consumer<HttpHeaders> oldHeaders() {
+		return httpHeaders -> {
+			httpHeaders.add(ORIGIN_HEADER, ORIGIN);
+			httpHeaders.add(ISSUER_HEADER, ISSUER);
 		};
 	}
 
 	private static SlackRequest addHeaderValues(SlackRequest request) {
 		return request.withOrigin(ORIGIN)
-			.withIssuer(ISSUER);
+			.withIssuer(X_SENT_BY_VALUE);
 	}
 }
